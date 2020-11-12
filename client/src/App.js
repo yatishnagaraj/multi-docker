6aa6745e7e514c79c6252c8f53cace254cd85bf1
YATISH NAGARAJ <yatishnagaraj@Simhasenappa.local>
2020-11-12 15:58:15 +0530

    changed home page

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Fibonacci numbers
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
